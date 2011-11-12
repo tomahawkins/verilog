d7e711ed793e58515ed4e3632787a83b33e90573
Tom Hawkins <tomahawkins@gmail.com>
2011-11-12 12:02:33 -0500

    Parser updates.

--- Language/Verilog/Types.hs
+++ Language/Verilog/Types.hs
@@ -1,5 +1,6 @@
 module Language.Verilog.Types
   ( Name
+  , Range
   , Module     (..)
   , ModuleItem (..)
   , Expr       (..)
@@ -7,20 +8,29 @@ module Language.Verilog.Types
 
 type Name = String
 
+type Range = (Expr, Expr)
+
 data Module = Module Name [ModuleItem] deriving (Show, Eq)
 
 data ModuleItem
   = Paremeter Name Expr
-  | Input     (Maybe (Expr, Expr)) [Name]
-  | Output    (Maybe (Expr, Expr)) [Name]
-  | Inout     (Maybe (Expr, Expr)) [Name]
-  | Wire      (Maybe (Expr, Expr)) [Name]
-  | Reg       (Maybe (Expr, Expr)) [Name]
+  | Input     (Maybe Range) [(Name, Maybe Range)]
+  | Output    (Maybe Range) [(Name, Maybe Range)]
+  | Inout     (Maybe Range) [(Name, Maybe Range)]
+  | Wire      (Maybe Range) [(Name, Maybe Range)]
+  | Reg       (Maybe Range) [(Name, Maybe Range)]
   deriving (Show, Eq)
 
 data Expr
   = String     String
   | Number     String
   | Identifier String
+  | Not        Expr
+  | And        Expr Expr
+  | Mul        Expr Expr
+  | Div        Expr Expr
+  | Mod        Expr Expr
+  | Add        Expr Expr
+  | Sub        Expr Expr
   deriving (Show, Eq)
 
